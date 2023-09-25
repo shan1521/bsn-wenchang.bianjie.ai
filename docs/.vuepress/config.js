9519967b044c5b9ecdc975ebe602e72f6b44a0fd
jinbiao-bianjie <zhangjinbiao9693@gmail.com>
2023-09-25 14:43:57 +0800

    update keywords

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    description: '「文昌链」基于边界智能的企业级联盟链产品 IRITA 打造，具备全生态商业服务能力的开放联盟链。以区块链跨链、NFT 技术及大数据隐私保护技术为核心，安全可控、符合国密标准，支持下一代分布式商业系统',
+    description: '文昌链基于边界智能自主研发的企业级联盟链产品 IRITA 打造而成，其生态包括版权保护、支付、物联网设备服务等数千个分布式商业应用，是具有全生态商业服务能力的开放许可链。文昌链在坚持开放与合规并行的发展原则下，持续探索区块链服务合规数字经济发展的创新路径。',
     base:'/',
     head: [
         [ 'link', {rel:'icon',href:'/favicon.ico'}],
-        [ 'meta', { name: 'keywords', content: '文昌链、文昌链官网、开放联盟链、开放联盟链框架、开放许可链 、IRITAOPB、iritaopb' }],
+        [ 'meta', { name: 'keywords', content: '文昌链,文昌链官网,文昌链网,文昌链极速网,文昌链天舟,文昌链天和,文昌链神舟,文昌链多链' }],
         [ 'meta', {property:'og:image',content:'https://www.bianjie.ai/resources/WenChangChain/Bianjie-Logo-WenChangChain-Vertical-Black.png'}],
         [ 'meta', {property:'og:title',content:'文昌链 - 由边界智能独立运营的开放联盟链'}],
         [ 'meta', {property:'og:description',content:'「文昌链」基于边界智能的企业级联盟链产品 IRITA 打造，具备全生态商业服务能力的开放联盟链。以区块链跨链、NFT 技术及大数据隐私保护技术为核心，安全可控、符合国密标准，支持下一代分布式商业系统'}],
